a3241817e42a359594699e1defaa601d1f00ac4c
Marco Hamann <marcohamann89@hotmail.de>
2018-10-19 22:32:10 +0200

    added test solution and projects ...

---
+++ SharpAkitaTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SharpAkitaTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("SharpAkitaTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1135adcf-67ec-47d6-a519-6ffba62a93d2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SharpAkitaTest/api/EntityStoreTest.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpAkita.Api.Store;
+
+namespace SharpAkitaTest.api
+{
+    [TestClass]
+    public class EntityStoreTest
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            var entityForCheck = new StoreTestEntity();
+            var store = new EntityStore<StoreTestEntity>();
+            store.EntityAdded += (o, e) => entityForCheck.StringValue = store.GetById(e).StringValue;
+
+            store.Add("1", new StoreTestEntity { IntValue = 1, StringValue = "Hello" });
+
+            Assert.AreEqual(1, store.Count);
+            Assert.AreEqual("Hello", entityForCheck.StringValue);
+        }
+
+        [TestMethod]
+        public void UpdateByPropertiesTest()
+        {
+            var entityForCheck = new StoreTestEntity();
+            var store = new EntityStore<StoreTestEntity>();
+            store.EntityChanged += (o, e) =>
+            {
+                entityForCheck.IntValue = store.GetById(e).IntValue;
+                entityForCheck.StringValue = store.GetById(e).StringValue;
+            };
+            store.Add("1", new StoreTestEntity { IntValue = 1, StringValue = "Hello" });
+            var valueDic = new Dictionary<string, object>
+            {
+                { nameof(StoreTestEntity.IntValue), 4 },
+                { nameof(StoreTestEntity.StringValue), "Hello world" }
+            };
+
+            store.UpdateByProperties("1", valueDic);
+
+            Assert.AreEqual(1, store.Count);
+            Assert.AreEqual(4, entityForCheck.IntValue);
+            Assert.AreEqual("Hello world", entityForCheck.StringValue);
+        }
+
+        [TestMethod]
+        public void UndoTest()
+        {
+            var store = new EntityStore<StoreTestEntity>();
+            store.Add("1", new StoreTestEntity { IntValue = 1, StringValue = "Hello" });
+
+            store.Undo();
+
+            Assert.AreEqual(0, store.Count);
+        }
+
+        [TestMethod]
+        public void RedoTest()
+        {
+            var entityForCheck = new StoreTestEntity();
+            var store = new EntityStore<StoreTestEntity>();
+            store.EntityAdded += (o, e) =>
+            {
+                entityForCheck.IntValue = store.GetById(e).IntValue;
+                entityForCheck.StringValue = store.GetById(e).StringValue;
+            };
+            store.Add("1", new StoreTestEntity { IntValue = 1, StringValue = "Hello" });
+            store.Undo();
+
+            store.Redo();
+
+            Assert.AreEqual(1, store.Count);
+            Assert.AreEqual("Hello", entityForCheck.StringValue);
+        }
+    }
+
+    public class StoreTestEntity
+    {
+        public int IntValue { get; set; }   
+
+        public string StringValue { get; set; }
+    }
+}
