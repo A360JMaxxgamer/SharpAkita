e45ca5a5497b0329e932c5adfeb9479ef8d4aca0
Marco Hamann <marcohamann89@hotmail.de>
2018-10-19 22:39:32 +0200

    added test cases

--- SharpAkitaTest/api/EntityStoreTest.cs
+++ SharpAkitaTest/api/EntityStoreTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SharpAkita.Api.Store;
 
@@ -72,6 +73,37 @@ namespace SharpAkitaTest.api
 
             Assert.AreEqual(1, store.Count);
             Assert.AreEqual("Hello", entityForCheck.StringValue);
+        }
+
+        [TestMethod]
+        public void GetByIdTest()
+        {
+            var test = new StoreTestEntity();
+            var test2 = new StoreTestEntity();
+            var store = new EntityStore<StoreTestEntity>();
+            store.Add("1", test);
+            store.Add("2", test2);
+
+            var result = store.GetById("2");
+            
+            Assert.AreEqual(test2, result);
+        }
+
+        [TestMethod]
+        public void SelectTest()
+        {
+            var test1 = new StoreTestEntity { IntValue = 1};
+            var test2 = new StoreTestEntity { IntValue = 1};
+            var test3 = new StoreTestEntity { IntValue = 2 };
+            var store = new EntityStore<StoreTestEntity>();
+            store.Add("1", test1);
+            store.Add("2", test2);
+            store.Add("3", test3);
+
+            var select = store.Select(e => e.IntValue == 1);
+
+            Assert.AreEqual(2, select.Count);
+            Assert.IsTrue(select.All(e => e.Value.IntValue == 1));
         }
     }
 
