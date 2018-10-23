72a555ed2dd37a013cf3bdba667b3e5c2ea347a5
Marco Hamann <marcohamann89@hotmail.de>
2018-10-23 19:37:03 +0200

    added implementation for entitystate and store base based on angular implementation

---
+++ SharpAkita/Api/Store/Store.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reactive.Subjects;
+
+namespace SharpAkita.Api.Store
+{
+    internal class Store<TState>
+    {
+        private BehaviorSubject<TState> store;
+
+        private TState currentStoreState;
+
+        //private ReplaySubject<Action> rootDispatcher = new ReplaySubject<Action>();
+
+        public Store()
+        {
+
+        }
+
+        public TState SetState(Func<TState, TState> newStateFunction)
+        {
+            var previousState = currentStoreState;
+            currentStoreState = newStateFunction(currentStoreState);
+
+            InitializeStore();
+            // Todo handle transaction in process
+            DispatchState(currentStoreState);
+        }
+
+        private void InitializeStore()
+        {
+            if (store == null)
+            {
+                store = new BehaviorSubject<TState>(currentStoreState);
+                // rootDispatcher.OnNext() Todo evaluate
+            }
+        }
+
+        private void DispatchState(TState state)
+        {
+            store.OnNext(state);
+        }
+    }
+}
