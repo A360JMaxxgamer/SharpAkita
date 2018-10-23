7210e579542da8393e97ff279ba077756e2c6197
Marco Hamann <marcohamann89@hotmail.de>
2018-10-23 22:01:40 +0200

    splitted external references into classes and added some test methods

--- SharpAkita/Extensions/ObjectExtensions.cs
+++ SharpAkita/Extensions/ObjectExtensions.cs
@@ -2,25 +2,24 @@
 
 using System.Collections.Generic;
 using System.Reflection;
-using System.ArrayExtensions;
 
 namespace System
 {
     public static class ObjectExtensions
     {
-        private static readonly MethodInfo CloneMethod = typeof(Object).GetMethod("MemberwiseClone", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static readonly MethodInfo CloneMethod = typeof(object).GetMethod("MemberwiseClone", BindingFlags.NonPublic | BindingFlags.Instance);
 
         public static bool IsPrimitive(this Type type)
         {
-            if (type == typeof(String)) return true;
+            if (type == typeof(string)) return true;
             return (type.IsValueType & type.IsPrimitive);
         }
 
-        public static Object Copy(this Object originalObject)
+        public static object Copy(this object originalObject)
         {
-            return InternalCopy(originalObject, new Dictionary<Object, Object>(new ReferenceEqualityComparer()));
+            return InternalCopy(originalObject, new Dictionary<object, object>(new ReferenceEqualityComparer()));
         }
-        private static Object InternalCopy(Object originalObject, IDictionary<Object, Object> visited)
+        private static object InternalCopy(object originalObject, IDictionary<object, object> visited)
         {
             if (originalObject == null) return null;
             var typeToReflect = originalObject.GetType();
@@ -31,7 +30,7 @@ namespace System
             if (typeToReflect.IsArray)
             {
                 var arrayType = typeToReflect.GetElementType();
-                if (IsPrimitive(arrayType) == false)
+                if (!IsPrimitive(arrayType))
                 {
                     Array clonedArray = (Array)cloneObject;
                     clonedArray.ForEach((array, indices) => array.SetValue(InternalCopy(clonedArray.GetValue(indices), visited), indices));
@@ -57,7 +56,7 @@ namespace System
         {
             foreach (FieldInfo fieldInfo in typeToReflect.GetFields(bindingFlags))
             {
-                if (filter != null && filter(fieldInfo) == false) continue;
+                if (filter != null && !filter(fieldInfo)) continue;
                 if (IsPrimitive(fieldInfo.FieldType)) continue;
                 var originalFieldValue = fieldInfo.GetValue(originalObject);
                 var clonedFieldValue = InternalCopy(originalFieldValue, visited);
@@ -66,68 +65,7 @@ namespace System
         }
         public static T Copy<T>(this T original)
         {
-            return (T)Copy((Object)original);
+            return (T)Copy((object)original);
         }
     }
-
-    public class ReferenceEqualityComparer : EqualityComparer<Object>
-    {
-        public override bool Equals(object x, object y)
-        {
-            return ReferenceEquals(x, y);
-        }
-        public override int GetHashCode(object obj)
-        {
-            if (obj == null) return 0;
-            return obj.GetHashCode();
-        }
-    }
-
-    namespace ArrayExtensions
-    {
-        public static class ArrayExtensions
-        {
-            public static void ForEach(this Array array, Action<Array, int[]> action)
-            {
-                if (array.LongLength == 0) return;
-                ArrayTraverse walker = new ArrayTraverse(array);
-                do action(array, walker.Position);
-                while (walker.Step());
-            }
-        }
-
-        internal class ArrayTraverse
-        {
-            public int[] Position;
-            private int[] maxLengths;
-
-            public ArrayTraverse(Array array)
-            {
-                maxLengths = new int[array.Rank];
-                for (int i = 0; i < array.Rank; ++i)
-                {
-                    maxLengths[i] = array.GetLength(i) - 1;
-                }
-                Position = new int[array.Rank];
-            }
-
-            public bool Step()
-            {
-                for (int i = 0; i < Position.Length; ++i)
-                {
-                    if (Position[i] < maxLengths[i])
-                    {
-                        Position[i]++;
-                        for (int j = 0; j < i; j++)
-                        {
-                            Position[j] = 0;
-                        }
-                        return true;
-                    }
-                }
-                return false;
-            }
-        }
-    }
-
 }
---
+++ SharpAkitaTest/Extensions/ObjectExtensionsTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SharpAkitaTest.Extensions
+{
+    public class ObjectExtensionsTest
+    {
+        [Fact]
+        public void CopyTest()
+        {
+            var toCopy = new A
+            {
+                AString = "Test",
+                B = new B
+                {
+                    BString = "Blub",
+                    Cs = new List<C>
+                    {
+                        new C { CInt =1 },
+                        new C { CInt = 5}
+                    }
+                }
+            };
+            toCopy.Self = toCopy;
+
+            var copy = toCopy.Copy();
+            toCopy.AString = "Not same reference";
+
+            Assert.NotEqual("Not same reference", copy.AString);
+            Assert.Equal("Test", copy.AString);
+            Assert.Equal("Blub", copy.B.BString);
+            Assert.Equal(1, copy.B.Cs[0].CInt);
+            Assert.Equal(5, copy.B.Cs[1].CInt);
+            Assert.Equal(2, copy.B.Cs.Count);
+        }
+    }
+
+    internal class A
+    {
+        public string AString { get; set; }
+
+        public B B { get; set; }
+
+        public A Self { get; set; }
+    }
+
+    internal class B
+    {
+        public string BString { get; set; }
+
+        public List<C> Cs { get; set; }
+    }
+
+    internal class C
+    {
+        public int CInt { get; set; }
+    }
+}
